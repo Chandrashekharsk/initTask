9f5e1db31ca80382e0a5fb25cb869299d7508b04
Chandrashekharsk <cs9981625252@gmail.com>
2025-01-17 18:21:24 +0530

    fix cors2

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
 const corsOptions = {
-  origin: process.env.FRONTEND_URL,
+  origin: `${process.env.FRONTEND_URL}`,
   credentials: true, 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
