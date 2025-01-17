5c71d29a6c92004d8d2bbb012cede54d294aee0b
Chandrashekharsk <cs9981625252@gmail.com>
2025-01-17 17:31:49 +0530

    uninstall bcrypt and installed bcryptjs

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import UserModel from "../models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import "dotenv/config";
 import crypto from "crypto";
